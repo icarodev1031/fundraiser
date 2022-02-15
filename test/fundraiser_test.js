984efe5dc8af964bd2596ee651f7953e70680680
doll <icarodev1031@gmail.com>
2022-02-15 14:50:55 +0900

    test3

--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -2,8 +2,8 @@ const FundraiserContract = artifacts.require("Fundraiser");
 contract("Fundraiser",accounts=>{
     console.log(accounts)
     let fundraiser;
-    const name = "Beneficiary Name";
-    const url = "beneficiaryname.org";
+    //const name = "Beneficiary Name";
+    //const url = "beneficiaryname.org";
     const imageUrl = "https://placekitten.com/600/350";
     const description = "Beneficiary description";
     //const beneficiary = accounts[1];
